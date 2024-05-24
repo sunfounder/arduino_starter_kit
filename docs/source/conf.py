7bb2f5479937f6204916a1536099c1e6e4c9e7f0
harumiume <127194455+harumiume@users.noreply.github.com>
2024-05-24 18:21:38 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -100,7 +100,7 @@ rst_epilog = """
 
 .. |link_sf_facebook| raw:: html
 
-    <a href="https://bit.ly/3wDYH38" target="_blank">here</a>
+    <a href="https://bit.ly/raphaelkit" target="_blank">here</a>
 
 
 .. |link_wiki_avometer| raw:: html
